9aa7a6479141beba143ab00e1048af4a17a10c57
David Muirhead <davidmuirhead@MacBookPro.gateway>
2026-01-07 17:20:56 +1000

    bruh

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ load_dotenv()
 TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID", "").strip()
 TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET", "").strip()
 
+FETCH_FIRST = int(os.getenv("FETCH_FIRST", "100"))
+
 GAME_NAME = os.getenv("GAME_NAME", "Among Us").strip()
 LOOKBACK_HOURS = int(os.getenv("LOOKBACK_HOURS", "24"))
 PARTNER_ONLY = os.getenv("PARTNER_ONLY", "true").lower() in ("1", "true", "yes", "y")
